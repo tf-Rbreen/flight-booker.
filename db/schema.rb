17d3ee50b722501cee9afc93298df262963db91b
tf-Rbreen <rbreen@tracefirst.com>
2023-10-26 15:48:20 +0100

    Passenger Migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_19_135619) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_26_144326) do
   create_table "bookings", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "flightid"
     t.string "fname"
@@ -27,6 +27,17 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_19_135619) do
     t.string "depairport"
     t.string "arrairport"
     t.datetime "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "passengers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "flightID"
+    t.string "name"
+    t.string "surname"
+    t.string "email"
+    t.string "passport_number"
+    t.date "date_of_birth"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
